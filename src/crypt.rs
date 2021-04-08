228bfae6c5358825841b2c23bcc95d41c872ca2c
Erik J. Sturcke <erik@sturcke.co>
2021-04-07 21:55:06 -0400

    Implement SHA1

--- src/crypt.rs
+++ src/crypt.rs
@@ -109,3 +109,84 @@ pub fn encrypt_mt(seed: u32, pt: &[u8]) -> Vec<u8> {
 pub fn decrypt_mt(seed: u32, ct: &[u8]) -> Vec<u8> {
   encrypt_mt(seed, ct)
 }
+
+const SHA1_LENGTH: usize = 20;
+
+pub fn sha1(message: &[u8]) -> [u8; SHA1_LENGTH] {
+  let f = |t: usize, b: u32, c: u32, d: u32| match t {
+    0..=19 => (b & c) | (!b & d),
+    20..=39 => b ^ c ^ d,
+    40..=59 => (b & c) | (b & d) | (c & d),
+    60..=79 => b ^ c ^ d,
+    _ => panic!("Invalid value {} for t", t),
+  };
+
+  let k = |t: usize| match t {
+    0..=19 => 0x5A827999u32,
+    20..=39 => 0x6ED9EBA1,
+    40..=59 => 0x8F1BBCDC,
+    60..=79 => 0xCA62C1D6,
+    _ => panic!("Invalid value {} for t", t),
+  };
+
+  let mut digest = [
+    0x67452301u32,
+    0xEFCDAB89,
+    0x98BADCFE,
+    0x10325476,
+    0xC3D2E1F0,
+  ];
+
+  // Pad the message and split into chunks to process
+  for m in [
+    message,
+    &[1u8 << 7],
+    &vec![0; 63 - ((message.len() + 8) % 64)][..],
+    &(message.len() as u64 * 8).to_be_bytes(),
+  ]
+  .concat()
+  .chunks(16 * 4)
+  {
+    let mut w = [0u32; 80];
+
+    // 1.a
+    for i in 0..16 {
+      w[i] = u32::from_be_bytes([m[i * 4], m[i * 4 + 1], m[i * 4 + 2], m[i * 4 + 3]]);
+    }
+
+    // 1.b
+    for t in 16..80 {
+      w[t] = (w[t - 3] ^ w[t - 8] ^ w[t - 14] ^ w[t - 16]).rotate_left(1);
+    }
+
+    // 1.c
+    let [mut a, mut b, mut c, mut d, mut e] = digest;
+
+    // 1.d
+    for t in 0..80 {
+      let temp = a.rotate_left(5) + f(t, b, c, d) + e + w[t] + k(t);
+      e = d;
+      d = c;
+      c = b.rotate_left(30);
+      b = a;
+      a = temp;
+    }
+
+    //  1.e.
+    digest[0] += a;
+    digest[1] += b;
+    digest[2] += c;
+    digest[3] += d;
+    digest[4] += e;
+  }
+
+  let mut result = [0u8; SHA1_LENGTH];
+  for (i, b) in digest
+    .iter()
+    .flat_map(|&w| w.to_be_bytes().to_vec())
+    .enumerate()
+  {
+    result[i] = b;
+  }
+  result
+}
